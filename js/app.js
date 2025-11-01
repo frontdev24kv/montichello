818a41a8b1f4495e41cc04bdd1db9be559da0488
frontdev24kv <kv.frontdev24@gmail.com>
2025-11-02 01:39:44 +0200

    created hero section

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 'use strict';
 
-const burger = document.querySelector('.burger')
+const burger = document.querySelector('.burger');
 burger.addEventListener('click', () => {
-  burger.classList.toggle('close')
+  burger.classList.toggle('close');
+});
+
+const dotButtons = document.querySelectorAll('.dot');
+const parent = document.getElementById('parent')
+
+parent.addEventListener('click', (e) => {
+  // if(e.target.classList.contains('active')){
+  //    e.target.classList.remove('active')
+  //    return
+  // }
+  dotButtons.forEach(btn => btn.classList.remove('active'))
+   e.target.classList.add('active')
 })
+
