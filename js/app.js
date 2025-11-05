1b95e54a9f001faab873a9c1ba13206508f3d2a3
frontdev24kv <kv.frontdev24@gmail.com>
2025-11-05 06:42:49 +0200

    created gallery section

--- js/app.js
+++ js/app.js
@@ -4,19 +4,38 @@ const burger = document.querySelector('.burger');
 burger.addEventListener('click', () => {
   burger.classList.toggle('close');
 });
-
+/*
 const dotButtons = document.querySelectorAll('.dot');
 const parent = document.getElementById('parent')
 
 parent.addEventListener('click', (e) => {
   console.log(e.target.tagName);
-  // if(e.target.tagName !== 'BUTTON') return
-  // if(e.target.classList.contains('active')){
-  //    e.target.classList.remove('active')
-  //    return
-  // }
+  if(e.target.tagName !== 'BUTTON') return
   
   dotButtons.forEach(btn => btn.classList.remove('active'))
-   e.target.classList.add('active')
+  e.target.classList.add('active')
 })
+*/
 
+
+// smooth scroll
+// $('.scroll-link').on('click', function (e) {
+//   e.preventDefault(); 
+//   const target = $(this).attr('href'); 
+
+//   $('html, body').animate({
+//     scrollTop: $(target).offset().top
+//   }, 2000); 
+// });
+
+$('.screen').slick({
+  dots: true,
+  arrows: false
+});
+
+$('.card__box').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  dots: true
+});
